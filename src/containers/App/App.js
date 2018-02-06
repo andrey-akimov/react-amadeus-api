ddfa7abd45cb2e2f195dc159deb9a27861acae75
Andrey Akimov <rojer9999@gmail.com>
2018-02-06 11:31:35 +0200

    a list bugs fixed

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,6 +5,7 @@ import Menu from '../../components/Menu';
 import Form from '../Form';
 import './App.css';
 import { connect } from 'react-redux';
+import * as _ from 'lodash';
 
 class App extends Component {
 	render() {
@@ -18,31 +19,44 @@ class App extends Component {
 						? this.props.hotels.map(
 								hotel => (
 									<div key={hotel.property_code}>
+										{/* IMG */}
 										<img
 											src="http://fakeimg.pl/350x200/ff0000/000"
 											alt="photos"
 										/>
+										{/* NAME AND ADDRESS */}
+
 										<h2>
 											{hotel.property_name},&nbsp;
 											<a href="/">
 												{hotel.address.line1}
 											</a>
 										</h2>
+
+										{/* CONTACTS */}
 										<p>
-											{hotel.contacts.map(contact => (
-												<span>{contact.detail}</span>
-											))}
+											{_.uniq(hotel.contacts).map(
+												contact => (
+													<span key={_.uniqueId()}>
+														{contact.detail}
+													</span>
+												)
+											)}
 										</p>
+
+										{/* DESCRIPTION */}
 										<p>
 											{hotel.amenities.map(amenity => (
-												<span>
+												<span key={_.uniqueId()}>
 													{amenity.description}
 												</span>
 											))}
 										</p>
+
+										{/* ROOMS FOR RENT */}
 										<p>
 											{hotel.rooms.map(room => (
-												<span>
+												<span key={_.uniqueId()}>
 													{
 														room.room_type_info
 															.room_type
@@ -50,6 +64,8 @@ class App extends Component {
 												</span>
 											))}
 										</p>
+
+										{/* PRICE */}
 										<p>
 											{hotel.total_price.amount}&nbsp;
 											{hotel.total_price.currency}
